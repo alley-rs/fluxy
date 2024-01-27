4a963ef2d44734339af8f304a10a52844e54c1dd
Ryan Wang <thepoy@163.com>
2024-01-27 22:05:52 +0800

    chore(static): 适配暗色样式

--- static/src/index.tsx
+++ static/src/index.tsx
@@ -4,6 +4,12 @@ import { render } from "solid-js/web";
 import "./index.scss";
 import App from "./App";
 
+if (matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.setAttribute("data-prefers-color-scheme", "dark");
+} else {
+  document.documentElement.setAttribute("data-prefers-color-scheme", "light");
+}
+
 const root = document.getElementById("root");
 
 render(() => <App />, root!);
