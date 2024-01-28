0c8cf1a10714c775d682a668d7ae7bba9bc9c782
Ryan Wang <thepoy@163.com>
2024-01-28 12:22:54 +0800

    feat(static): 添加暗色模式手动切换

--- static/src/App.tsx
+++ static/src/App.tsx
@@ -1,8 +1,10 @@
+import { Match, Switch, createSignal, onMount } from "solid-js";
+import { BiRegularSun, BiSolidMoon } from "solid-icons/bi";
 import Result from "~/components/result";
 import Send from "./pages/send";
 import Receive from "./pages/receive";
+import SwitchDark from "./components/switch";
 import "./App.scss";
-import { Match, Switch } from "solid-js";
 
 type Mode = "receive" | "send";
 
@@ -12,26 +14,57 @@ const App = () => {
   const params = new URLSearchParams(url.search);
   const mode = params.get("mode") as Mode | null;
 
+  const [isDark, setIsDark] = createSignal(false);
+
+  onMount(() => {
+    if (matchMedia("(prefers-color-scheme: dark)").matches) {
+      setIsDark(true);
+    } else {
+      setIsDark(false);
+    }
+
+    // 监听系统颜色切换
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        if (event.matches) {
+          setIsDark(true);
+        } else {
+          setIsDark(false);
+        }
+      });
+  });
+
   return (
-    <Switch>
-      <Match when={!mode}>
-        <div class="container result">
-          <Result
-            status="error"
-            title="无效的请求"
-            description="缺少查询参数: mode"
-          />
-        </div>
-      </Match>
+    <div class={isDark() ? "dark" : undefined}>
+      <SwitchDark
+        class="dark-switch"
+        checked={isDark()}
+        setChecked={() => setIsDark((pre) => !pre)}
+        uncheckedChild={<BiRegularSun />}
+        checkedChild={<BiSolidMoon />}
+      />
 
-      <Match when={mode === "send"}>
-        <Send />
-      </Match>
+      <Switch>
+        <Match when={!mode}>
+          <div class="container result">
+            <Result
+              status="error"
+              title="无效的请求"
+              description="缺少查询参数: mode"
+            />
+          </div>
+        </Match>
 
-      <Match when={mode === "receive"}>
-        <Receive />
-      </Match>
-    </Switch>
+        <Match when={mode === "send"}>
+          <Send />
+        </Match>
+
+        <Match when={mode === "receive"}>
+          <Receive />
+        </Match>
+      </Switch>
+    </div>
   );
 };
 
