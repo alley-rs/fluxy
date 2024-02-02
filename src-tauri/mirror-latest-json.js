612ce0516bad69c5431ba52de43da38b03bfedfa
Ryan Wang <thepoy@163.com>
2024-02-02 19:42:37 +0800

    chore(action): 调试工作流

--- src-tauri/mirror-latest-json.js
+++ src-tauri/mirror-latest-json.js
@@ -2,36 +2,8 @@ import { getOctokit } from "@actions/github";
 import fs from "fs";
 import path from "path";
 
-module.exports = async ({ release_id, context }) => {
-  const github = getOctokit(process.env.GITHUB_TOKEN);
-
-  const { owner, repo } = context.repo;
-
-  const versionFilename = "latest.json";
-
-  const assets = await github.rest.repos.listReleaseAssets({
-    owner: owner,
-    repo: repo,
-    release_id,
-    per_page: 50,
-  });
-  const asset = assets.data.find((e) => e.name === versionFilename);
-
-  if (!asset) throw new Error("latest.js was not found in release assets");
-
-  const data = await github.request(
-    "GET /repos/{owner}/{repo}/releases/assets/{asset_id}",
-    {
-      owner: owner,
-      repo: repo,
-      asset_id: asset.id,
-      headers: {
-        accept: "application/octet-stream",
-      },
-    },
-  ).data;
-
-  const text = Buffer.from(data).toString();
+const run = async ({ release_id, context }) => {
+  const text = process.env.TEXT;
   const mirror = text.replaceAll(
     "https://github.com/",
     "https://kkgithub.com/",
@@ -39,7 +11,7 @@ module.exports = async ({ release_id, context }) => {
 
   const dir = process.cwd();
 
-  const filepath = path.join(dir, versionFilename);
+  const filepath = path.join(dir, "latest.json");
 
   console.log("mirror latest json path: ", filepath);
 
