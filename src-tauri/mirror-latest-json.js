71d04c4ac510065488297af6ab409350b76dc0e0
Ryan Wang <thepoy@163.com>
2024-02-03 10:44:25 +0800

    chore(action): 调试 action

--- src-tauri/mirror-latest-json.js
+++ src-tauri/mirror-latest-json.js
@@ -3,22 +3,22 @@ import path from "path";
 
 const run = async () => {
   const text = process.env.TEXT;
-  let mirror = text.replaceAll(
-    "https://github.com/",
-    "https://kkgithub.com/",
-  );
+  let mirror = text.replaceAll("https://github.com/", "https://kkgithub.com/");
 
   // 删除开头和结尾的引号
   if (mirror[0] === '"') {
-    mirror = mirror.slice(1)
+    mirror = mirror.slice(1);
   }
 
   if (mirror[mirror.length - 1] === '"') {
-    mirror = mirror.slice(0, mirror.length -1);
+    mirror = mirror.slice(0, mirror.length - 1);
   }
 
   // 删除换行符, 多个空格合并为一个空格
-  mirror = mirror.replaceAll("\n", '').replaceAll(/\s+/g, ' ');;
+  mirror = mirror
+    .replaceAll("\\n", "")
+    .replaceAll('\\"', '"')
+    .replaceAll(/\s+/g, " ");
 
   const dir = process.cwd();
 
