5cf27092ddfae900e15e3bb00154bb0a1f634f7b
Ryan Wang <thepoy@163.com>
2024-02-03 23:06:34 +0800

    chore(action): 更换 github 镜像

--- src-tauri/mirror-latest-json.js
+++ src-tauri/mirror-latest-json.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const run = async () => {
   const text = process.env.TEXT;
-  let mirror = text.replaceAll("https://github.com/", "https://kkgithub.com/");
+  let mirror = text.replaceAll("https://github.com/", "https://hub.yzuu.cf/");
 
   // 删除开头和结尾的引号
   if (mirror[0] === '"') {
