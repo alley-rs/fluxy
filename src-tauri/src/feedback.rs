f3a34bf812919d2d4bdd48f9d6d26cbfa771200f
Ryan Wang <thepoy@163.com>
2024-07-06 16:52:22 +0800

    chore(src-tauri): 添加一些日志

--- src-tauri/src/feedback.rs
+++ src-tauri/src/feedback.rs
@@ -9,12 +9,22 @@ use crate::{error::FluxyResult, lazy::APP_CONFIG_DIR};
 pub async fn get_star_state() -> FluxyResult<bool> {
     let file_path = APP_CONFIG_DIR.join("STAR");
     if !file_path.exists() || file_path.is_dir() {
+        info!("star 文件不存在");
         return Ok(false);
     }
 
-    let mut file = File::open(file_path).await?;
+    let mut file = File::open(&file_path).await.map_err(|e| {
+        error!(
+            message = "只读打开文件时出错",
+            path = ?file_path,
+            error = ?e
+        );
+        e
+    })?;
     let mut buf = [0u8; 1];
     file.read_exact(&mut buf).await?;
+
+    info!(message = "保存的 star 状态", state = buf[0] == 1);
 
     Ok(buf[0] == 1)
 }
@@ -22,8 +32,19 @@ pub async fn get_star_state() -> FluxyResult<bool> {
 #[tauri::command]
 pub async fn stared() -> FluxyResult<()> {
     let file_path = APP_CONFIG_DIR.join("STAR");
-    let mut file = File::create(file_path).await?;
-    file.write_all(&[1]).await?;
+    let mut file = File::create(&file_path).await.map_err(|e| {
+        error!(
+            message = "写入打开文件时出错",
+            path = ?file_path,
+            error = ?e
+        );
+        e
+    })?;
+    file.write_all(&[1]).await.map_err(|e| {
+        error!(message = "向文件写入数据时出错", error = ?e);
+        e
+    })?;
+    info!("已保存 star 状态");
 
     Ok(())
 }
